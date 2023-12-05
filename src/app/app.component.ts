fdaef26920f0fabc5339e62ed36c7dfcc381992f
Justin-Barlowe <jbarlowe@my365.bellevue.edu>
2023-12-05 10:53:27 -0500

    Capstone Project

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+// Name: Justin Barlowe
+// File: app.component.ts
+// Description: App component file for the loan application.
+// Date: 12/4/2023
+// Angular Version: 16.2.10
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'loan-app';
+}
